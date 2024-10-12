d11f4c2275de3e55b9bd63873cb0a772a9461838
dongapple <hg971027@gmail.com>
2024-10-13 04:45:25 +0900

    🎉 프리티어,esLint 충돌 해결 #1

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  overrides: [
+    {
+      files: ['*.js'],
+      excludedFiles: '.eslintrc.js',
+      env: {
+        node: true,
+      },
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': ['warn'],
+    'no-console': 'warn',
+    'prettier/prettier': 'error',
+  },
+};
