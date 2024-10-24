f2c35b5eb355c94230bdf3155a4eaa1c5058dcee
dongapple <hg971027@gmail.com>
2024-10-24 19:18:27 +0900

    ✨ 저장된 키 불러오기 #2

--- src/components/keyPress/KeyPress.tsx
+++ src/components/keyPress/KeyPress.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import KeyboardButton from './KeyboardButton';
 import MouseButton from './MouseButton';
+import useStore from '../../services/store';
 
 const KeyPress: React.FC = () => {
+  const keyBindings = useStore((state) => state.keyBindings);
+
   return (
     <>
-      <KeyboardButton keyName="w" />
-      <KeyboardButton keyName="a" />
-      <KeyboardButton keyName="s" />
-      <KeyboardButton keyName="d" />
-      <KeyboardButton keyName=" " />
-      <MouseButton buttonName="left" keyNumber={0} />
-      <MouseButton buttonName="right" keyNumber={2} />
+      <KeyboardButton keyName={keyBindings.jump} />
+      <KeyboardButton keyName={keyBindings.moveLeft} />
+      <KeyboardButton keyName={keyBindings.crouch} />
+      <KeyboardButton keyName={keyBindings.moveRight} />
+      <KeyboardButton keyName={keyBindings.gather} />
+      <MouseButton buttonName="attack" keyNumber={keyBindings.attack} />
+      <MouseButton buttonName="skill" keyNumber={keyBindings.skill} />
     </>
   );
 };
